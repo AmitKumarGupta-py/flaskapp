b2d1dd74a34ec0f5db9e37828014e6dcebda3710
AmitKumarGupta-py <amitgupta.pyz@gmail.com>
2024-12-27 18:46:37 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 
 def hello():
-    return "Its Lunch Time.. empty stomach does not digest anything... especially study!!!"
+    return "Welcome to Jenkins!!!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
